42a61b107df4de4b34f04865cb8a17ef91cb27fb
Arnold Pajuelo <arnold.pajuelo@gmail.com>
2021-11-26 00:33:13 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# To-Do_List
+ Small To-Do List app in Python
---
+++ main.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+root = Tk()
+root.resizable(False,False)
+root.title("To-Do List")
+
+
+mainFrame = Frame(root)
+mainFrame.config(width = 400, height = 300)
+mainFrame.pack()
+
+
+root.mainloop()
